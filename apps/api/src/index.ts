ef0844f43428dc010f7fc2c83fe0c336d20ce3a1
Nick Clark <nick.clark@dovetailstudios.com>
2022-10-04 19:42:21 +1300

    Add another arg

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,9 @@ import { log } from "logger";
 
 const port = process.env.PORT || 3001;
 const server = createServer();
+const myVar = process.env.MY_VAR;
 
 server.listen(port, () => {
   log(`api running on ${port}`);
+  log(`myVar = ${myVar}`);
 });
