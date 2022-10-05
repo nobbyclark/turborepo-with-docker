53e1d8a472d8e8c3594beb80e9df17dd63b41ac3
Nick Clark <nick.clark@dovetailstudios.com>
2022-10-05 14:06:29 +1300

    Fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,4 +3,8 @@ const path = require("path");
 
 module.exports = withTM({
   reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 });
